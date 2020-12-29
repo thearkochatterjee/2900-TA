b7211781516224aee90c5ed54ba9ea9317f86ee7
Arko Chatterjee <arkochatterjee1006@gmail.com>
2020-12-29 11:22:18 -0500

    Testing Signal Input

--- air motor simulartor/Test Serial.py
+++ air motor simulartor/Test Serial.py
@@ -6,5 +6,7 @@ time.sleep(2)
 for i in range(0,100):
     b = ser.readline()
     b = b.decode('utf-8').strip()
-    print(b)
+    pindisp = int(b.split(",")[0][1:])
+    signaldisp = b.split(",")[1]
+    print(pindisp)
 ser.close()
